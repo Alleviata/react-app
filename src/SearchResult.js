df2f989b2328466fade824bff5c8ccb1c9649de4
Alleviata <yuliya.honyaku@gmail.com>
2023-01-02 21:15:53 +0200

    add phonetics

--- src/SearchResult.js
+++ src/SearchResult.js
@@ -1,13 +1,19 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
 
 export default function SearchReasult(props) {
   if (props.result) {
-    console.log(props.result);
     return (
       <div className="result">
         <h2>{props.result.word}</h2>
-
+        {props.result.phonetics.map(function (phonetic, index) {
+          return (
+            <div key={index}>
+              <Phonetic phonetic={phonetic} />
+            </div>
+          );
+        })}
         {props.result.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
