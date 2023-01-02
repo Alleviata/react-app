a48365072cebd1519222c83555d3082f1f9a4fcf
Alleviata <yuliya.honyaku@gmail.com>
2023-01-03 00:26:55 +0200

    added photos

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,11 +2,14 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import SearchResult from "./SearchResult";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
   let [word, setWord] = useState(props.word);
   let [result, setResult] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState([]);
+
   function changeWord(event) {
     setWord(event.target.value);
   }
@@ -14,9 +17,19 @@ export default function Dictionary(props) {
     setResult(response.data[0]);
   }
 
+  function handleImages(response) {
+    setPhotos(response.data.photos);
+  }
+
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelUrl = `https://api.pexels.com/v1/search?query=${word}&per_page=12`;
+    let apiKey = "563492ad6f9170000100000165cf7b733690428b91fcc1dfc3af8b1d";
+    axios
+      .get(pexelUrl, { headers: { Authorization: `Bearer ${apiKey}` } })
+      .then(handleImages);
   }
 
   function handleSubmit(event) {
@@ -43,6 +56,7 @@ export default function Dictionary(props) {
           </form>
           <SearchResult result={result} />
         </section>
+        <Photos photos={photos} />
       </div>
     );
   } else {
